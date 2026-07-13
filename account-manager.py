1976e8cb972bb65804446b47e6251ddc4ab25532
MikiYas <aklilemikiyas@gmail.com>
2026-07-12 23:19:51 -0700

    delete the manager file

--- account-manager.py
+++
@@ -1,9 +0,0 @@
-class Account:
-    def __init__(self, owner, bal):
-        self.owner = owner
-        self.balance = bal
-    def deposit(self, amount):
-        self.balance += amount
-        return (owner,bal)
-a = Account("Almaz", 1500)
-a.deposit(500)
